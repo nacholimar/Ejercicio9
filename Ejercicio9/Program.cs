d4a3c7b2afa53777a8d9d2e1cad3c15dcdadde59
Ignacio Castillo <82484760+nacholimar@users.noreply.github.com>
2022-06-15 18:21:50 -0300

    ejercicio

--- Ejercicio9/Program.cs
+++ Ejercicio9/Program.cs
@@ -10,6 +10,34 @@ namespace Ejercicio9
     {
         static void Main(string[] args)
         {
+            int[] myArray = { 1, 2, 2, 5, 4, 6, 7, 8, 8, 8 };
+
+            int valor = myArray[0];
+            int rep = 1;
+            int maxRep = 1;
+            int number = 0;
+
+            for (int item = 1; item < myArray.Length; item++)
+            {
+                if (myArray[item] == valor)
+                {
+                    rep++;
+                }
+                else
+                {
+                    rep = 1;
+                }
+
+                if (rep > maxRep)
+                {
+                    maxRep = rep;
+                    number = myArray[item];
+                }
+                valor = myArray[item];
+            }
+            Console.WriteLine("Longest: " + maxRep);
+            Console.WriteLine("Number: " + number);
+            Console.ReadLine();
         }
     }
 }
